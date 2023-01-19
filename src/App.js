b3463a8483e65213aee82d8a8fe476e903882f0b
Araceligm90 <garcia.araceli90@gmail.com>
2023-01-19 17:52:15 -0500

    lab3 finished

--- src/App.js
+++ src/App.js
@@ -3,15 +3,55 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import "./css/App.css"
+import Modal from 'react-bootstrap/Modal';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      beastName: '',
+      beastImage: ''
+    }
+  }
+
+  handleShowModal = (name, beastPic) => {
+    this.setState({
+      showModal: true,
+      beastName: name,
+      beastImage: beastPic
+    })
+  }
+
+  handleCloseModal = () => {
+    this.setState({
+      showModal: false
+    })
+  }
+
+
+
   render() {
     return (
       <>
-      <Header />
-      <Main />
-      <Footer />
+        <Header />
+        <Main
+          handleShowModal={this.handleShowModal}
+        />
+        <Footer />
+        <Modal
+          show={this.state.showModal}
+          onHide={this.handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {this.state.beastName}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={this.state.beastImage} alt={''} height={250}/>
+          </Modal.Body>
+        </Modal>
       </>
     );
   }
